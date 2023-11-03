7965ec7d04f47e30b4084b726fead9f4be775e31
luciolanguyen <chanhphat630@gmail.com>
2023-11-03 11:00:47 +0700

    add check node working correctly

--- Subspace/update_subspace.sh
+++ Subspace/update_subspace.sh
@@ -6,7 +6,17 @@ cd $HOME/subspace
 wget -O subspace-node https://github.com/subspace/subspace/releases/download/gemini-3g-2023-oct-31/subspace-node-ubuntu-x86_64-skylake-gemini-3g-2023-oct-31
 wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/gemini-3g-2023-oct-31/subspace-farmer-ubuntu-x86_64-skylake-gemini-3g-2023-oct-31
 cp subspace-* /usr/local/bin/
-systemctl status subspace-farmerd
-sleep 5
-systemclt status subspace-noded
-sleep 5
+
+#check node and farm working correctly 
+if systemctl is-active --quiet subspace-noded.service; then
+  # If the service is active, print success message in green color
+  echo -e "\e[32mSubspace node started successfully\e[0m"
+else
+  # If the service is not active, print failure message in red color
+  echo -e "\e[31mSubspace node failed to start\e[0m"
+fi
+sleep 3
+echo "#######################################################"
+echo "journalctl -u subspace-noded -f --no-hostname | ccze -A"
+echo "to check log"
+echo "#######################################################"
